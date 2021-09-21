63c67507353039c6f941fa5752e2d9fb5615e59c
Gothalion <mbarsukovv@gmail.com>
2021-09-21 15:44:34 +0300

    Fix: BAM, slider width, validator errors

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
-
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  allowTouchMove: true,
   // Keyboard arrow control
   keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
 
+  simulateTouch: true,
+  touchRatio: 1,
+  touchAngle: 45,
+  grabCursor: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button__next",
-    prevEl: ".slider-button__prev",
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
 });
