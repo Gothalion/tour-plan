dae6af7fe28864cd443cda7c88e8aa501ef61354
Gothalion <mbarsukovv@gmail.com>
2021-09-22 22:31:08 +0300

    Create: added yandex maps

--- js/main.js
+++ js/main.js
@@ -20,3 +20,11 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".slider-button--prev",
   },
 });
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7,
+  });
+}
