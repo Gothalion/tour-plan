d30dead7aa11f2890bf3408aa281f91f591444d6
Gothalion <mbarsukovv@gmail.com>
2021-10-03 22:35:19 +0300

    Create: modal window

--- js/main.js
+++ js/main.js
@@ -1,42 +1,60 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  allowSlideNext: true,
-  allowSlidePrev: true,
-  allowTouchMove: true,
-  // Keyboard arrow control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    allowTouchMove: true,
+    // Keyboard arrow control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
 
-  simulateTouch: true,
-  touchRatio: 1,
-  touchAngle: 45,
-  grabCursor: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-});
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  allowSlideNext: true,
-  allowSlidePrev: true,
-  allowTouchMove: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    grabCursor: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+  });
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    allowTouchMove: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom__visible");
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom__visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
